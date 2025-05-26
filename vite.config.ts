8afea664b676afcc95a3b5a4a167d151b447e546
Utkarsh Gautam <89135822+utkarshg12@users.noreply.github.com>
2025-05-27 02:45:17 +0530

    ✨ Updated frontend code

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ts.ispat/', // 🔥 THIS LINE FIXES THE PROBLEM
+  base: '/ts.ispat.db/', // 🔥 THIS LINE FIXES THE PROBLEM
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
